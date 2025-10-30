37b7e3fc7d0d67efcc86ae9c9ec0382e9e1b9909
Abdullah Jawed <abdullahjawed2021@gmail.com>
2025-10-30 15:05:09 +0530

    Add files via upload

---
+++ js Types/js arrays.js
@@ -0,0 +1,47 @@
+// JavaScript Array
+// An array is an object that can store multiple values at once.
+
+const age = [17, 18, 15, 19, 14];
+// In the above example, we created an array to record the age of five students.
+
+
+
+// Arrays in js
+
+
+// let person = ["sohail",93939939,10,true,null,undefined]
+// note : arrays uses indexing in js 
+// Arrays starts from 0
+// using the index number you can perform crud operations
+
+// how to access any value from array (using index)
+// index starts from 0
+
+
+let person = ["sohail",93939939,10,true,null,undefined]
+//                0         1    2   3   4     5
+
+//accessing array Values
+
+
+// Accessing Array values
+// console.log(person[0], person[1]);
+// console.log(person[4])
+
+// Adding a new value in array
+// console.log(person);
+// person[6] = "ali";
+// console.log(person);
+
+// Updating an array
+
+// console.log(person);
+// person[2] = 40;
+// person[3] = false;
+// console.log(person);
+
+
+// deleting an array
+console.log(person);
+delete person[0];
+console.log(person);    
